a6407ea48528a21cd418e955786b5d50c4056073
Lorenzo Torres <lorenzoajt@gmail.com>
2023-09-19 22:59:26 -0600

    nirvana2-dev

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-d-*qr#j!0j3q*5f$-u1j1x+@8kimtmbqkkihme##**@$6&hvic
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'nirvana-dev.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'nirvana2-dev.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
